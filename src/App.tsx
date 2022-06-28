d9219017ef5b747b170c1176c8c3e5c90358e401
Indrakant Mishra <indrakantm23@gmail.com>
2022-06-28 15:19:24 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <label htmlFor="">Continent:</label>
+      <select name="" id="">
+        <option value=""></option>
+      </select>
+    </div>
+  );
+}
+
+export default App;
